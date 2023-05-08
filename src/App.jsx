f1040d283409cb0b5596b06f8b3938d65cc4211c
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-09 02:32:57 +0300

    implement google login

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,56 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from "react";
+import { googleLogout, useGoogleLogin } from "@react-oauth/google";
+import axios from "axios";
 import './App.css'
-import googleOneTap from 'google-one-tap'
-
-const options = {
-  client_id: import.meta.env.VITE_CLIENT_ID,
-  auto_select: false,
-  cancel_on_tap_outside: false,
-  context: "signin"
-}
 
 function App() {
-  const [loginData, setLoginData] = useState(
-    localStorage.getItem("userCreds")
-    ? JSON.parse(localStorage.getItem("userCreds"))
-    : null
-  )
+  const [user, setUser] = useState(null);
+  const [profile, setProfile] = useState(null);
+
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => setUser(codeResponse),
+    onError: (error) => console.log("Login Failed:", error),
+  });
 
   useEffect(() => {
-    if(!loginData) {
-      googleOneTap(options, async (res) => {
-        console.log(res)
-      })
+    if (user) {
+      axios
+        .get(
+          `https://www.googleapis.com/oauth2/v1/userinfo?access_token=${user.access_token}`,
+          {
+            headers: {
+              Authorization: `Bearer ${user.access_token}`,
+              Accept: "application/json",
+            },
+          }
+        )
+        .then((res) => {
+          setProfile(res.data);
+        })
+        .catch((err) => console.log(err));
     }
-  }, [])
+  }, [user]);
+
+  const logOut = () => {
+    googleLogout();
+    setProfile(null);
+  };
 
   return (
     <div>
-      Hello
+      <h2>React Google Login</h2>
+      {profile ? (
+        <div>
+          <img src={profile.picture} alt="user image" />
+          <h3>User Logged in</h3>
+          <p>Name: {profile.name}</p>
+          <p>Email Address: {profile.email}</p>
+          <button onClick={logOut}>Log out</button>
+        </div>
+      ) : (
+        <button onClick={() => login()}>Sign in with Google 🚀 </button>
+      )}
     </div>
-  )
+  );
 }
-
-export default App
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { GoogleOAuthProvider } from '@react-oauth/google';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+  <App />
+  </GoogleOAuthProvider>,
 )
