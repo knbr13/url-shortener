02887199f7b3b553d7189ab237507642978a5c08
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-09 04:27:10 +0300

    create cards data

---
+++ src/data/cards.js
@@ -0,0 +1,12 @@
+export const cards = [
+  { src: "../assets/images/gary.webp" },
+  { src: "../assets/images/karen.webp" },
+  { src: "../assets/images/mrKrabs.webp" },
+  { src: "../assets/images/mrsPuff.webp" },
+  { src: "../assets/images/patrick.webp" },
+  { src: "../assets/images/pearl.webp" },
+  { src: "../assets/images/plankton.webp" },
+  { src: "../assets/images/sandy.webp" },
+  { src: "../assets/images/spongeBob.webp" },
+  { src: "../assets/images/squidward.webp" },
+];
