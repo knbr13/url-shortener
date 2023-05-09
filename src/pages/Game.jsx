fa52b24ef90adb576116a4e75ac639b64e1ddb50
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-09 04:53:07 +0300

    create the game board

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -1,9 +1,12 @@
+import Board from "../components/Board"
 import Navbar from "../components/Navbar"
 
 const Game = () => {
   return (
-    <div className="p-1">
+    <div className="p-1 flex flex-col gap-2">
         <Navbar />
+        <h2 className="w-full text-center text-gray-800 text-2xl font-bold">Match with SpongeBob</h2>
+        <Board />
     </div>
   )
 }
