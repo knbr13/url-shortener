c9b0d338cf7c28f3732e0eb78a4661c483d2cf69
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-10 02:39:16 +0300

    implement API functions in order to increase the reuseability of the code

---
+++ src/api/userAPI.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const userAPI = axios.create({ baseURL: "http://localhost:4000/user" });
+
+export const addUser = (data) => userAPI.post("/", data);
+export const updateScore = (data) => userAPI.put("/score", data);
+export const getUsersWithHighestScores = (data) =>
+  userAPI.get("/", { params: { ...data } });
