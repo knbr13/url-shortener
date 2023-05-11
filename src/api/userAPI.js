df8c320e627c2dd38bf741b6e328b7758cd060f7
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-12 02:28:21 +0300

    replace baseURL with the deployed one

--- src/api/userAPI.js
+++ src/api/userAPI.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const userAPI = axios.create({ baseURL: "http://localhost:4000/user" });
+const userAPI = axios.create({ baseURL: "https://worrisome-fox-tank-top.cyclic.app/user" });
 
 export const addUser = (data) => userAPI.post("/", data);
 export const updateScore = (data) => userAPI.put("/score", data);
