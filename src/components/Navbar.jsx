aa09524e7bf992e0fc1b11bff81ecd0b498faa31
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-11 22:42:34 +0300

    use UserProfile component in Navbar component, edit the styles for UserProfile component

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,14 @@
 import { Link } from "react-router-dom"
+import UserProfile from "./UserProfile"
 
 const Navbar = () => {
   return (
-    <div className="flex justify-around text-black bg-opacity-50 bg-gray-300 w-10/12 m-auto rounded-md">
+    <div className="flex">
+      <div className="flex justify-around text-black bg-opacity-50 bg-gray-300 w-10/12 m-auto rounded-md">
         <Link className="text-gray-800" to={"/game"}>Game</Link>
         <Link className="text-gray-800" to={"/leaderboard"}>leaderboard</Link>
+      </div>
+      <UserProfile />
     </div>
   )
 }
--- src/components/UserProfile.jsx
+++ src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-const UserProfile = ({user}) => {
-  return (
-    <div>
-        <img src={user.picture} className="rounded-full w-12 md:w-16" />
-    </div>
-  )
-}
+import { useContext } from "react";
+import { UserContext } from "../context/userContext";
 
-export default UserProfile
+const UserProfile = () => {
+  const userCreds = useContext(UserContext);
+  return (
+    <div className="bg-gray-300 bg-opacity-80 rounded-2xl p-1 cursor-pointer">
+      <img src={userCreds.user.picture} className="rounded-full w-10 md:w-12" />
+    </div>
+  );
+};
+
+export default UserProfile;
