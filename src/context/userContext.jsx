89d71eb9afa4dbd3bc44e078519009bca64199fd
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-11 22:29:13 +0300

    create User Context

---
+++ src/context/userContext.jsx
@@ -0,0 +1,17 @@
+import { createContext, useState } from "react";
+const UserContext = createContext();
+
+const UserContextWrapper = ({ children }) => {
+  const [user, setUser] = useState(
+    localStorage.getItem("userCreds")
+      ? JSON.parse(localStorage.getItem("userCreds"))
+      : null
+  );
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserContextWrapper;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { GoogleOAuthProvider } from '@react-oauth/google';
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+import UserContextWrapper from "./context/userContext.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
-  <App />
-  </GoogleOAuthProvider>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <UserContextWrapper>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENT_ID}>
+      <App />
+    </GoogleOAuthProvider>
+  </UserContextWrapper>
+);
