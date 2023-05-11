02a85e7fa98c931a681b890ef970e543d69ed83d
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-11 19:21:55 +0300

    modify the styles of the ranking numbers in Score component

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -4,11 +4,11 @@ const Score = ({ user, index, scoreField }) => {
       className="flex relative justify-around items-center border-b p-1 border-gray-500"
       key={index}
     >
-      <p className="absolute left-2 text-gray-800 md:text-xl">{index + 1}</p>
+      <p className="absolute left-1 top-1 bg-gray-300 px-1 rounded-full text-gray-800 md:text-xl">{index + 1}</p>
       <img src={user.picture} className="rounded-full w-12 md:w-16" />
       <div className="bg-gray-300 rounded-lg px-2 bg-opacity-80">
-        <p className="text-lg md:text-2xl text-gray-800">{user.name}</p>
-        <p className="text-sm md:text-lg text-gray-700">{user.email}</p>
+        <p className="text-sm sm:text-lg md:text-xl text-gray-800">{user.name}</p>
+        <p className="text-xs sm:text:sm md:text-base text-gray-700">{user.email}</p>
       </div>
       <p className="text-xl md:text-3xl text-gray-800 bg-gray-300 rounded-lg px-2 bg-opacity-80">
         {scoreField == "timeScore" ? user.timeScore : user.flipsScore}
