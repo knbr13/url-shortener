9db8ba2e7fd7f6b8197dc074f29e9f436147c462
abdullah-alaadine <abdullahalaadine63@gmail.com>
2023-05-18 22:43:01 +0300

    remove displaying emails on the leaderboard

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -10,7 +10,6 @@ const Score = ({ user, index, scoreField, currentPage }) => {
       <img src={user.picture} className="rounded-full w-10 md:w-14" />
       <div className="bg-gray-300 w-6/12 overflow-x-hidden rounded-lg px-2 bg-opacity-80">
         <p className="text-sm sm:text-lg md:text-xl text-black">{user.name}</p>
-        <p className="text-xs sm:text:sm md:text-base text-gray-700 break-words">{user.email}</p>
       </div>
       <p className="text-[10px] sm:text-xs md:text-sm lg:text-base w-2/12 text-gray-800 break-words bg-gray-300 rounded-lg px-2 bg-opacity-80">
         {scoreField == "timeScore" ? formatTime(user.timeScore) : user.flipsScore}
--- src/components/ScoresBoard.jsx
+++ src/components/ScoresBoard.jsx
@@ -7,7 +7,7 @@ const ScoresBoard = ({ users, totalPages, onPageChange, currentPage, scoreField,
     <div className="flex flex-col gap-2 m-auto w-11/12 text-center md:w-9/12 lg:w-7/12 rounded-lg p-1 bg-gray-300 bg-opacity-50">
       <div className="flex relative justify-around items-center border-b border-gray-800">
         <p className="text-sm md:text-lg text-gray-800">Picture</p>
-        <p className="text-sm md:text-lg text-gray-800">name + email</p>
+        <p className="text-sm md:text-lg text-gray-800">name</p>
         <p className="text-sm md:text-lg text-gray-800">{scoreField === "timeScore" ? "time": "flips"}</p>
       </div>
       {users?.map((user, index) => {
