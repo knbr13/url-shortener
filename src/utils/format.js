216a8ca9758e8481239477c78f903f8ca996b2f6
knbr13 <abdullahalaadine63@gmail.com>
2024-05-25 09:28:36 +0300

    reset

--- src/utils/format.js
+++
@@ -1,8 +0,0 @@
-export const formatTime = (seconds) => {
-  const minutes = Math.floor(seconds / 60);
-  const remainingSeconds = seconds % 60;
-  let str = "";
-  minutes ? (str += `${minutes} min`) : null;
-  remainingSeconds ? (str += ` ${remainingSeconds} sec`) : null;
-  return str;
-};
